ece075c185437e2c3ce56b1e56e06726e7548928
ShamanNark <7301878@gmail.com>
2016-09-20 00:21:09 +0300

    Thread-safe queue implementation

---
+++ QueueExtended/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace QueueExtended
+{
+    static class Program
+    {
+        static QueueExtended<int> Element = new QueueExtended<int>();
+        static void Main(string[] args)
+        {
+
+            Thread thread1 = new Thread(Test_pop);
+            thread1.Start();
+            Thread thread2 = new Thread(Test_push);
+            thread2.Start();            
+
+            Console.ReadKey();
+        }
+
+        public static void Test_pop()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Element.Pop());
+            }
+
+        }
+
+        public static void Test_push()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Element.Push(i);
+            }
+        }
+    }
+}
---
+++ QueueExtended/QueueExtended.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace QueueExtended
+{
+    internal class QueueExtended<T>
+    {
+        private readonly Queue<T> queue = new Queue<T>();
+
+        public T Pop()
+        {
+            lock (queue)
+            {
+                while (queue.Count == 0)
+                {
+                    Monitor.Wait(queue);
+                }
+
+                return queue.Dequeue();
+            }
+        }
+
+        public void Push(T data)
+        {
+            lock (queue)
+            {
+                queue.Enqueue(data);
+                Console.WriteLine("записал число {0}", data);
+                Monitor.Pulse(queue);
+            }
+        }
+    }
+}
